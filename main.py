41035c63ca57d9ead1adb2ed38a95a68d9883f86
acrostorn <50541746+acrostorn@users.noreply.github.com>
2025-01-09 10:50:55 +0900

    1/9 2nd

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 """
 安藤式制御
-FIXME: 置く位置によって初期条件設定 仮にWWで設定中
+FIXME: 置く位置によって初期条件設定
 """
 
-#制御パラメータ定義
-timeInterval = 1000/60             # (1s/frameRatio) [ms]
+#制御パラメータ定数定義
+timeInterval = 1000/60             # (1s/frameRate) [ms]
 gain = 1             
 defaultPower = 100
 initialDelta = 100
-deltaScaleRate = 0.1               #per frame
-#maxPower = 200
-#minPower = 50
+deltaScaleRate = 0.1               #微分値(d/df)
+maxPower = 200
+minPower = 0
+emergencyRate = 100000
 
 # 条件式定義
 WW = Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE)
@@ -21,14 +22,20 @@ BB = Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) an
 #変数定義
 deltaScale = 0
 consecutiveCount = 0
+isEmergency = False
 latestState = "WW" if WW else "WB" if WB else "BW" if BW else "BB"    #TODO: enumで書き直す
 
 def update():
+    #TODO: WWととBBの扱い
     currentState = "WW" if WW else "WB" if WB else "BW" if BW else "BB"
 
-    #TODO: WWととBBの扱いは別にすべきかも
-    #TODO: maxSpeed, minSpeedの適用
-    #FIXME: WWが一定の期間続いたらエラーを出さなあかんね
+    if isEmergency:
+        return
+
+    #エラー処理処理
+    if consecutiveCount > emergencyRate:
+        emergency()
+
     if currentState == latestState :
         consecutiveCount += 1
         deltaScale += deltaScaleRate if WB else -deltaScaleRate if BW else 0
@@ -37,10 +44,22 @@ def update():
         latestState = currentState
         deltaScale = 0
 
-def main():
-    rightPower = defaultPower + gain * (deltaScale*initialDelta)
-    leftPower = defaultPower - gain * (deltaScale*initialDelta)
-    #stop関数作った方がいい
+def mainLoop():
+    if not isEmergency:
+        leftPower = defaultPower + gain * (deltaScale*initialDelta)
+        rightPower = defaultPower - gain * (deltaScale*initialDelta)
+        
+        if leftPower >= maxPower:
+            leftPower = maxPower
+        elif leftPower <= minPower:
+            leftPower = minPower
+        
+        if rightPower >= maxPower:
+            rightPower = maxPower
+        elif rightPower <= minPower:
+            rightPower = minPower
+    else:
+        leftPower, rightPower = 0, 0
 
     move(leftPower, rightPower)
 
@@ -61,15 +80,27 @@ def move(leftPower: Float, rightPower: Float):
     #method3
     Tinybit.car_ctrl_speed2(Tinybit.CarState.CAR_RUN, leftPower, rightPower)
 
-#def sound():
+#エラー回復
+def recoverEmergency():
+    isEmergency = False
 
 #エラー処理
-"""Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
+def emergency():
+    isEmergency = True
+    Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
     music.set_volume(40)
     music.play(music.string_playable("C5 F C5 F C5 F C5 F ", 120),
         music.PlaybackMode.UNTIL_DONE)
-    basic.show_string("SOS")"""
+    basic.show_string("SOS")
+
+#def sound():
 
 #flowEntry
+basic.forever(mainLoop)
 loops.every_interval(timeInterval, update)
-basic.forever(main)
+
+"""
+Q
+・move method blockについて
+
+"""
--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 /** 
 安藤式制御
-FIXME: 置く位置によって初期条件設定 仮にWWで設定中
+FIXME: 置く位置によって初期条件設定
 
  */
-// 制御パラメータ定義
+// 制御パラメータ定数定義
 let timeInterval = 1000 / 60
-//  (1s/frameRatio) [ms]
+//  (1s/frameRate) [ms]
 let gain = 1
 let defaultPower = 100
 let initialDelta = 100
 let deltaScaleRate = 0.1
-// per frame
-// maxPower = 200
-// minPower = 50
+// 微分値(d/df)
+let maxPower = 200
+let minPower = 0
+let emergencyRate = 100000
 //  条件式定義
 let WW = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)
 let WB = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)
@@ -21,6 +22,7 @@ let BB = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black)
 // 変数定義
 let deltaScale = 0
 let consecutiveCount = 0
+let isEmergency = false
 let latestState = WW ? "WW" : (WB ? "WB" : (BW ? "BW" : "BB"))
 // TODO: enumで書き直す
 function move(leftPower: number, rightPower: number) {
@@ -42,23 +44,61 @@ function move(leftPower: number, rightPower: number) {
     Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, leftPower, rightPower)
 }
 
-// def sound():
+// エラー回復
+function recoverEmergency() {
+    let isEmergency = false
+}
+
 // エラー処理
-/** Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
-    music.set_volume(40)
-    music.play(music.string_playable("C5 F C5 F C5 F C5 F ", 120),
-        music.PlaybackMode.UNTIL_DONE)
-    basic.show_string("SOS")
- */
+function emergency() {
+    let isEmergency = true
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Stop, 0)
+    music.setVolume(40)
+    music.play(music.stringPlayable("C5 F C5 F C5 F C5 F ", 120), music.PlaybackMode.UntilDone)
+    basic.showString("SOS")
+}
+
+// def sound():
 // flowEntry
+basic.forever(function mainLoop() {
+    let leftPower: number;
+    let rightPower: number;
+    if (!isEmergency) {
+        leftPower = defaultPower + gain * (deltaScale * initialDelta)
+        rightPower = defaultPower - gain * (deltaScale * initialDelta)
+        if (leftPower >= maxPower) {
+            leftPower = maxPower
+        } else if (leftPower <= minPower) {
+            leftPower = minPower
+        }
+        
+        if (rightPower >= maxPower) {
+            rightPower = maxPower
+        } else if (rightPower <= minPower) {
+            rightPower = minPower
+        }
+        
+    } else {
+        let [leftPower, rightPower] = [0, 0]
+    }
+    
+    move(leftPower, rightPower)
+})
 loops.everyInterval(timeInterval, function update() {
     let consecutiveCount: number;
     let deltaScale: number;
     let latestState: any;
+    // TODO: WWととBBの扱い
     let currentState = WW ? "WW" : (WB ? "WB" : (BW ? "BW" : "BB"))
-    // TODO: WWととBBの扱いは別にすべきかも
-    // TODO: maxSpeed, minSpeedの適用
-    // FIXME: WWが一定の期間続いたらエラーを出さなあかんね
+    if (isEmergency) {
+        return
+    }
+    
+    // エラー処理処理
+    if (consecutiveCount > emergencyRate) {
+        emergency()
+    }
+    
     if (currentState == latestState) {
         consecutiveCount += 1
         deltaScale += WB ? deltaScaleRate : (BW ? -deltaScaleRate : 0)
@@ -69,9 +109,9 @@ loops.everyInterval(timeInterval, function update() {
     }
     
 })
-basic.forever(function main() {
-    let rightPower = defaultPower + gain * (deltaScale * initialDelta)
-    let leftPower = defaultPower - gain * (deltaScale * initialDelta)
-    // stop関数作った方がいい
-    move(leftPower, rightPower)
-})
+/** 
+Q
+・move method blockについて
+
+
+ */
