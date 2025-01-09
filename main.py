fa3fcc125887cb4495702375cf7e0f3735f2985e
acrostorn <50541746+acrostorn@users.noreply.github.com>
2025-01-09 10:04:47 +0900

    1/9 1st

--- main.py
+++ main.py
@@ -1,50 +1,75 @@
-timeInterval = 500 #ms
+"""
+安藤式制御
+FIXME: 置く位置によって初期条件設定 仮にWWで設定中
+"""
 
+#制御パラメータ定義
+timeInterval = 1000/60             # (1s/frameRatio) [ms]
+gain = 1             
+defaultPower = 100
+initialDelta = 100
+deltaScaleRate = 0.1               #per frame
+#maxPower = 200
+#minPower = 50
 
-a = 0
-b = 0
-c = 0
-d = 0
+# 条件式定義
+WW = Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE)
+WB = Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK)
+BW = Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE)
+BB = Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK)
 
-def on_every_interval():
-    global a, b, c, d
-    if Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE):
-        a += 1
-    elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK):
-        b += 1
-    elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE):
-        c += 1
+#変数定義
+deltaScale = 0
+consecutiveCount = 0
+latestState = "WW" if WW else "WB" if WB else "BW" if BW else "BB"    #TODO: enumで書き直す
+
+def update():
+    currentState = "WW" if WW else "WB" if WB else "BW" if BW else "BB"
+
+    #TODO: WWととBBの扱いは別にすべきかも
+    #TODO: maxSpeed, minSpeedの適用
+    #FIXME: WWが一定の期間続いたらエラーを出さなあかんね
+    if currentState == latestState :
+        consecutiveCount += 1
+        deltaScale += deltaScaleRate if WB else -deltaScaleRate if BW else 0
     else:
-        d += 1
-loops.every_interval(timeInterval, on_every_interval)
+        consecutiveCount = 0
+        latestState = currentState
+        deltaScale = 0
 
-def on_forever():
-    global a, b, c
-    if Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE):
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_RUN, 50)
-    elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.WHITE) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.BLACK):
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINRIGHT, 60)
-    elif Tinybit.Line_Sensor(Tinybit.enPos.LEFT_STATE, Tinybit.enLineState.BLACK) and Tinybit.Line_Sensor(Tinybit.enPos.RIGHT_STATE, Tinybit.enLineState.WHITE):
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINLEFT, 60)
-    else:
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
-        music.set_volume(81)
-        music.play(music.string_playable("C5 F C5 F C5 F C5 F ", 120),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.show_string("SOS")
-    if a >= 20:
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
-        music.set_volume(81)
-        music.play(music.string_playable("C5 F C5 F C5 F C5 F ", 120),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.show_string("SOS")
-        a = 0
-    elif b >= 5:
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINRIGHT, 60)
-        b = 0
-    elif c >= 5:
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINLEFT, 60)
-        c = 0
-    else:
-        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_RUN, 50)
-basic.forever(on_forever)
+def main():
+    rightPower = defaultPower + gain * (deltaScale*initialDelta)
+    leftPower = defaultPower - gain * (deltaScale*initialDelta)
+    #stop関数作った方がいい
+
+    move(leftPower, rightPower)
+
+def move(leftPower: Float, rightPower: Float):
+    #method1
+    """powerGap = leftPower - rightPower
+    if powerGap == 0:
+        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_RUN, leftPower)
+        basic.show_arrow(ArrowNames.NORTH)
+    elif powerGap > 0:
+        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINRIGHT, powerGap)
+        basic.show_arrow(ArrowNames.NORTH_EAST)
+    elif powerGap < 0:
+        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINLEFT, abs(powerGap))
+        basic.show_arrow(ArrowNames.NORTH_WEST)"""
+    #method2
+    """Tinybit.car_sport(leftPower, rightPower)"""
+    #method3
+    Tinybit.car_ctrl_speed2(Tinybit.CarState.CAR_RUN, leftPower, rightPower)
+
+#def sound():
+
+#エラー処理
+"""Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
+    music.set_volume(40)
+    music.play(music.string_playable("C5 F C5 F C5 F C5 F ", 120),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.show_string("SOS")"""
+
+#flowEntry
+loops.every_interval(timeInterval, update)
+basic.forever(main)
--- main.ts
+++ main.ts
@@ -1,51 +1,77 @@
-let timeInterval = 500
-// ms
-let a = 0
-let b = 0
-let c = 0
-let d = 0
-loops.everyInterval(timeInterval, function on_every_interval() {
-    
-    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        a += 1
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        b += 1
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        c += 1
+/** 
+安藤式制御
+FIXME: 置く位置によって初期条件設定 仮にWWで設定中
+
+ */
+// 制御パラメータ定義
+let timeInterval = 1000 / 60
+//  (1s/frameRatio) [ms]
+let gain = 1
+let defaultPower = 100
+let initialDelta = 100
+let deltaScaleRate = 0.1
+// per frame
+// maxPower = 200
+// minPower = 50
+//  条件式定義
+let WW = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)
+let WB = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)
+let BW = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)
+let BB = Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)
+// 変数定義
+let deltaScale = 0
+let consecutiveCount = 0
+let latestState = WW ? "WW" : (WB ? "WB" : (BW ? "BW" : "BB"))
+// TODO: enumで書き直す
+function move(leftPower: number, rightPower: number) {
+    // method1
+    /** powerGap = leftPower - rightPower
+    if powerGap == 0:
+        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_RUN, leftPower)
+        basic.show_arrow(ArrowNames.NORTH)
+    elif powerGap > 0:
+        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINRIGHT, powerGap)
+        basic.show_arrow(ArrowNames.NORTH_EAST)
+    elif powerGap < 0:
+        Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_SPINLEFT, abs(powerGap))
+        basic.show_arrow(ArrowNames.NORTH_WEST)
+ */
+    // method2
+    /** Tinybit.car_sport(leftPower, rightPower) */
+    // method3
+    Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, leftPower, rightPower)
+}
+
+// def sound():
+// エラー処理
+/** Tinybit.car_ctrl_speed(Tinybit.CarState.CAR_STOP, 0)
+    music.set_volume(40)
+    music.play(music.string_playable("C5 F C5 F C5 F C5 F ", 120),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.show_string("SOS")
+ */
+// flowEntry
+loops.everyInterval(timeInterval, function update() {
+    let consecutiveCount: number;
+    let deltaScale: number;
+    let latestState: any;
+    let currentState = WW ? "WW" : (WB ? "WB" : (BW ? "BW" : "BB"))
+    // TODO: WWととBBの扱いは別にすべきかも
+    // TODO: maxSpeed, minSpeedの適用
+    // FIXME: WWが一定の期間続いたらエラーを出さなあかんね
+    if (currentState == latestState) {
+        consecutiveCount += 1
+        deltaScale += WB ? deltaScaleRate : (BW ? -deltaScaleRate : 0)
     } else {
-        d += 1
+        consecutiveCount = 0
+        latestState = currentState
+        deltaScale = 0
     }
     
 })
-basic.forever(function on_forever() {
-    
-    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 50)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 60)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 60)
-    } else {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Stop, 0)
-        music.setVolume(81)
-        music.play(music.stringPlayable("C5 F C5 F C5 F C5 F ", 120), music.PlaybackMode.UntilDone)
-        basic.showString("SOS")
-    }
-    
-    if (a >= 20) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Stop, 0)
-        music.setVolume(81)
-        music.play(music.stringPlayable("C5 F C5 F C5 F C5 F ", 120), music.PlaybackMode.UntilDone)
-        basic.showString("SOS")
-        a = 0
-    } else if (b >= 5) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 60)
-        b = 0
-    } else if (c >= 5) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 60)
-        c = 0
-    } else {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 50)
-    }
-    
+basic.forever(function main() {
+    let rightPower = defaultPower + gain * (deltaScale * initialDelta)
+    let leftPower = defaultPower - gain * (deltaScale * initialDelta)
+    // stop関数作った方がいい
+    move(leftPower, rightPower)
 })
